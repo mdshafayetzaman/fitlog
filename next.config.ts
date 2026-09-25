f42b1bc4fd689997564a5f20aeb3d3f3cdcb3a54
Md Ratul <mdratul@Mds-MacBook-Air.local>
2026-09-26 02:00:37 +0600

    add ignoreBuildErrors to next config

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,5 @@
 import type { NextConfig } from 'next'
-
+//
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
@@ -14,4 +14,4 @@ const nextConfig: NextConfig = {
   },
 }
 
-export default nextConfig
+export default nextConfig;
