46bb36c2ec62b862b87a6df8993079822538b146
Md Ratul <mdratul@Mds-MacBook-Air.local>
2026-09-26 14:44:38 +0600

    fix: dynamic base URL for fetch and fallback to prevent build failure

--- src/app/lib/api.tsx
+++ src/app/lib/api.tsx
@@ -1,23 +1,27 @@
+const getBaseUrl = () => {
+
+  if (process.env.VERCEL_URL) {
+    return `https://${process.env.VERCEL_URL}`
+  }
+
+  return process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000'
+}
+
 export const getLibrary = async () => {
   try {
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_API_URL}/data.json`,
-      {
-        next: {
-          revalidate: 15,
-        },
+    const response = await fetch(`${getBaseUrl()}/data.json`, {
+      next: {
+        revalidate: 15,
       },
-    )
+    })
 
     if (!response.ok) {
       throw new Error('Failed to fetch data')
     }
 
-    const data = await response.json()
-
-    return data
+    return await response.json()
   } catch (error) {
     console.error('Error fetching library:', error)
-    throw new Error('Failed to fetch library data')
+    return
   }
 }
